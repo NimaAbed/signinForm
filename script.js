c2935475f4e37f3df3c84f51f9b9f85b04350c3f
Nima Abed <abodnima84@gmail.com>
2023-06-24 15:30:41 +0330

    Create Func And Get Data

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ const userMsg = document.querySelector(".username-msg")
 const passMsg = document.querySelector(".password-msg")
 const successfullyMsg = document.querySelector(".signin-status")
 submit.addEventListener("click", btnHandler)
+
+function btnHandler(){
+    if (input.value.includes("@") && input.value.includes(".") && pass.value.length >= 4) {
+        console.log("hamechi ali")
+        userMsg.innerHTML = ""
+        passMsg.innerHTML = ""
+        let userData = {
+            email: input.value,
+            password: pass.value,
+        }
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify(userData),
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        })
+            .then(Response => {
+                if (Response.ok) {
+                    successfullyMsg.innerHTML = "You signed in successfully"
+                }
+            })
+
+
+    } else {
+
+    }
+}
